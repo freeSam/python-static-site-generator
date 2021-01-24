a2c0e8023a8c86d11fd6f1df921637b3617c6f47
Shamsher Singh <sssidhu753@gmail.com>
2021-01-24 17:31:23 +0530

    Fix open()

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -11,15 +11,15 @@ class Parser():
         return extension in self.extensions
 
     def parse(self, path: Path, source: Path, dest: Path):
-        raise NotImplementedError("Not Implemented")
+        raise NotImplementedError
 
     def read(self, path: Path) -> str:
-        with path.open() as file:
+        with open(path, 'r') as file:
             return file.read()
 
     def write(self, path: Path, dest: Path, content: str, ext: str = ".html"):
         full_path = dest / path.with_suffix(ext).name
-        with full_path.open() as file:
+        with open(full_path,  'w') as file:
             file.write(content)
 
     def copy(self, path: Path, source: Path, dest: Path):
