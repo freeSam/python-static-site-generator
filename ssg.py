8af2afd02d13469ed924bf60b55855ddb0224341
Shamsher Singh <sssidhu753@gmail.com>
2021-01-30 20:51:04 +0530

    Module 4

--- ssg.py
+++ ssg.py
@@ -5,7 +5,9 @@ import ssg.parsers
 
 def main(source: str = "content", dest: str = "dist") -> None:
     config = {"source": source, "dest": dest,
-              "parsers": [ssg.parsers.ResourceParser()]}
+              "parsers": [ssg.parsers.ResourceParser(),
+                          ssg.parsers.MarkdownParser(),
+                          ssg.parsers.ReStructuredTextParser()]}
     Site(**config).build()
 
 
